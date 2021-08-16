4733465082d201f0694722567d2a9948f941da51
Fabio Prudencio <fabio.oliveira.prudencio@gmail.com>
2021-08-16 09:49:28 -0300

    estrutura do while

--- src/curso_java_completo/EstruturasRepeticao.java
+++ src/curso_java_completo/EstruturasRepeticao.java
@@ -6,29 +6,45 @@ import java.util.Scanner;
 public class EstruturasRepeticao {
 
 	public void executarExemplo() {
-		
-		estruturaFor();
-		//estruturaWhile();
+
+		estruturaDoWhile();
+		// estruturaFor();
+		// estruturaWhile();
 	}
 
-	private void estruturaWhile() {  //recomendado usar while quando não se sabe quantas vezes será repetido
+	private void estruturaDoWhile() {
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		int x = sc.nextInt();		
+		char resp;
+		do {
+			System.out.print("Digite a temperatura em Celsius: ");
+			double C = sc.nextDouble();
+			double F = 9.0 * C / 5.0 + 32.0;
+			System.out.printf("Equivalente em Fahrenheit: %.1f%n", F);
+			System.out.print("Deseja repetir (s/n)? ");
+			resp = sc.next().charAt(0);
+		} while (resp != 'n');
+		sc.close();
+
+	}
+
+	private void estruturaWhile() { // recomendado usar while quando não se sabe quantas vezes será repetido
+		Scanner sc = new Scanner(System.in);
+
+		int x = sc.nextInt();
 		int soma = 0;
 		while (x != 0) {
 			x = sc.nextInt();
 			soma += x;
-		}		
+		}
 		sc.close();
 		System.out.println("reultado da Soma " + soma);
 	}
-	
-	private void estruturaFor() { //recomendado utilizar para quando se sabe o número de vezes que vai executar		
-		for (int i=4; i>=0; i--) {
-			System.out.println("Valor de i: " + i);		
-		}		
+
+	private void estruturaFor() { // recomendado utilizar para quando se sabe o número de vezes que vai executar
+		for (int i = 4; i >= 0; i--) {
+			System.out.println("Valor de i: " + i);
+		}
 	}
-	
 
 }
