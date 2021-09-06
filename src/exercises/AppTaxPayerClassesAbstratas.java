9d42d555f9bacd5efb408f40e6f58edcf4ce9798
Fabio Prudencio <fabio.oliveira.prudencio@gmail.com>
2021-09-06 15:27:52 -0300

    Exercício de classes abstratas e polimorfismo

---
+++ src/exercises/AppTaxPayerClassesAbstratas.java
@@ -0,0 +1,55 @@
+package exercises;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Company;
+import entities.Individual;
+import entities.TaxPayer;
+
+public class AppTaxPayerClassesAbstratas {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List<TaxPayer> list = new ArrayList<>();
+		System.out.print("Enter the number of tax payers: ");
+		int n = sc.nextInt();
+		
+		for (int i = 1; i <= n; i++) {
+			System.out.println("Tax payer #"+i+" data:");
+			System.out.print("Individual or company (i/c)? ");
+			char ch = sc.next().charAt(0);
+			sc.nextLine();
+			System.out.print("Name: ");
+			String name = sc.nextLine();
+			System.out.print("Anual income: ");
+			double anualIncome = sc.nextDouble();
+			if (ch == 'i') {
+				System.out.print("Health expenditures: ");
+				double healthExpenditures = sc.nextDouble();
+				list.add(new Individual(name, anualIncome, healthExpenditures));
+			} else {
+				System.out.print("Number of employees: ");
+				int numberOfEmployees = sc.nextInt();
+				list.add(new Company(name, anualIncome, numberOfEmployees));
+			}				
+		}
+		
+		System.out.println();
+		System.out.println("TAXES PAID:");
+		Double sum = 0.0;
+		for (TaxPayer payer : list){
+			System.out.println(String.format("%s: $%.2f", payer.getName(), payer.tax()));
+			sum += payer.tax(); 
+		}
+		
+		System.out.println();
+		System.out.println(String.format("TOTAL TAXES: $%.2f", sum));
+	}
+
+}
