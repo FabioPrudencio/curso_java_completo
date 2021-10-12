a8e23d921b3ce4f64601e660f033c78a98f17e06
Fabio Prudencio <fabio.oliveira.prudencio@gmail.com>
2021-10-12 14:40:21 -0300

    Usos da classe Comparator

---
+++ src/exercises/AppComparator1Products.java
@@ -0,0 +1,26 @@
+package exercises;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.entities.Product;
+
+public class AppComparator1Products {
+
+	public static void main(String[] args) {
+
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Notebook", 1200.00));
+		list.add(new Product("Tablet", 450.00));
+
+		list.sort(new MyComparator());
+
+		for (Product p : list) {
+			System.out.println(p);
+		}
+
+	}
+
+}
---
+++ src/exercises/AppComparator2ProductsClasseAnonima.java
@@ -0,0 +1,34 @@
+package exercises;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import model.entities.Product;
+
+public class AppComparator2ProductsClasseAnonima {
+
+	public static void main(String[] args) {
+
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Notebook", 1200.00));
+		list.add(new Product("Tablet", 450.00));
+
+		Comparator<Product> comp = new Comparator<Product>() {
+			@Override
+			public int compare(Product p1, Product p2) {
+				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+			}
+		};
+
+		list.sort(comp);
+
+		for (Product p : list) {
+			System.out.println(p);
+		}
+
+	}
+
+}
